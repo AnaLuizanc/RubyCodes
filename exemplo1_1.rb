c91ed2268e7a0f2edcca1c33f275b91a23c3fbc6
AnaLuizanc <analuizanc13@gmail.com>
2025-05-11 21:17:10 -0300

    atualização do código de construção nova

--- exemplo1_1.rb
+++ exemplo1_1.rb
@@ -1,6 +1,9 @@
 # Exemplo 1: mostrando construção nova da linguagem
 
-idade = 23
+print "Qual o seu nome? "
+nome = gets.chomp.to_s
+print "Qual a sua idade? "
+idade = gets.chomp.to_i
 case idade
 when 0..17
   puts "Menor de idade";
@@ -9,6 +12,5 @@ when 18..59
 else
   puts "Idoso"
 end
-
 
 
