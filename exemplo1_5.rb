90125b8556ec13719b27c0c9b9f83574d439cee4
AnaLuizanc <analuizanc13@gmail.com>
2025-05-12 08:03:49 -0300

    algoritmo 3 de falta de ortogonalidade e teste

---
+++ exemplo1_5.rb
@@ -0,0 +1,10 @@
+# Algoritmo de amostragem de falta de ortogonalidade 3
+# Regra: Em Ruby, é possível inserir o valor de uma variável
+#        dentro de uma string. Em outras palavras, uma variável pode
+#        ser interpolada em uma string, permitindo que seu conteúdo
+#        seja exibido diretamente no texto.
+# Exceção: Exceto, se a string for representada por aspas simples (‘ ‘)
+
+$linguagem = "Ruby"
+puts "Olá, estamos programando em #{$linguagem}."
+puts 'Olá, estamos programando em #{$linguagem}.'
---
+++ exemplo1_5lua.rb
@@ -0,0 +1,11 @@
+# Algoritmo de amostragem de falta de ortogonalidade 3
+# Regra: Em Ruby, é possível inserir o valor de uma variável
+#        dentro de uma string. Em outras palavras, uma variável pode
+#        ser interpolada em uma string, permitindo que seu conteúdo
+#        seja exibido diretamente no texto.
+# Exceção: Exceto, se a string for representada por aspas simples (‘ ‘)
+# Teste: Testar interpolação com variável global.
+
+$nome = 'Luciana'
+puts "Bom dia, #{$nome}."
+puts 'Bom dia, #{$nome}.'
